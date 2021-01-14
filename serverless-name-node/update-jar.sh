dbf73829880f59a072f09c32aa4aaf4d00d18571
Ben <benrcarver@gmail.com>
2021-01-14 17:59:49 +0000

    Added to scripts to update JAR after building (add config files) and update openwhisk func

---
+++ serverless-name-node/update-jar.sh
@@ -0,0 +1,3 @@
+zip -u /home/ben/ServerlessNameNode/serverless-name-node/target/serverless-name-node-1.0-SNAPSHOT.jar /home/ben/ServerlessNameNode/serverless-name-node/core-site.xml 
+zip -u /home/ben/ServerlessNameNode/serverless-name-node/target/serverless-name-node-1.0-SNAPSHOT.jar /home/ben/ServerlessNameNode/serverless-name-node/hdfs-site.xml 
+zip -u /home/ben/ServerlessNameNode/serverless-name-node/target/serverless-name-node-1.0-SNAPSHOT.jar /home/ben/ServerlessNameNode/serverless-name-node/ndb-config.properties
