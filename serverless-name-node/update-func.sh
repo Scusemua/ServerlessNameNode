d32069ed698dd533286b2c9933fd78c3c07a91cd
Ben <benrcarver@gmail.com>
2021-01-18 21:25:50 +0000

    Added 'config' folder to house the configuration files for HopsFS and updated update-jar script.

--- serverless-name-node/update-func.sh
+++ serverless-name-node/update-func.sh
@@ -1,1 +1,1 @@
-wsk -i action update namenode target/serverless-name-node-1.0-SNAPSHOT.jar --main org.apache.hadoop.hdfs.server.namenode.ServerlessNameNode --docker scusemua/java8action --memory 512
+wsk -i action update namenode target/serverless-name-node-1.0-SNAPSHOT.jar --main org.apache.hadoop.hdfs.server.namenode.ServerlessNameNode --docker scusemua/java8action --memory 1024
--- serverless-name-node/update-jar.sh
+++ serverless-name-node/update-jar.sh
@@ -1,3 +1,1 @@
-zip -u /home/ben/ServerlessNameNode/serverless-name-node/target/serverless-name-node-1.0-SNAPSHOT.jar /home/ben/ServerlessNameNode/serverless-name-node/core-site.xml 
-zip -u /home/ben/ServerlessNameNode/serverless-name-node/target/serverless-name-node-1.0-SNAPSHOT.jar /home/ben/ServerlessNameNode/serverless-name-node/hdfs-site.xml 
-zip -u /home/ben/ServerlessNameNode/serverless-name-node/target/serverless-name-node-1.0-SNAPSHOT.jar /home/ben/ServerlessNameNode/serverless-name-node/ndb-config.properties
+cd /home/ben/ServerlessNameNode/serverless-name-node/config && zip -ur /home/ben/ServerlessNameNode/serverless-name-node/target/serverless-name-node-1.0-SNAPSHOT.jar .
