d5daa9ad8835b045c49321d3918a0443ebaa5cf3
Scusemua <benrcarver@gmail.com>
2021-02-17 17:09:22 -0500

    Removed call to .setActiveNameNodes() in acquireLock for create transaction.

--- serverless-name-node/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNameSystem.java
+++ serverless-name-node/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNameSystem.java
@@ -2257,7 +2257,7 @@ public class FSNameSystem implements NameSystem, FSNameSystemMBean, NameNodeMXBe
                     LockFactory lf = getInstance();
                     INodeLock il = lf.getINodeLock(INodeLockType.WRITE_ON_TARGET_AND_PARENT, INodeResolveType.PATH, src)
                             .setNameNodeID(nameNode.getId())
-                            .setActiveNameNodes(nameNode.getActiveNameNodes().getActiveNodes())
+                            //.setActiveNameNodes(nameNode.getActiveNameNodes().getActiveNodes())
                             .skipReadingQuotaAttr(!dir.isQuotaEnabled());
                     locks.add(il);
                     locks.add(lf.getEZLock());
